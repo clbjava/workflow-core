7c379a722d10c6073b170d4711aae113fa87dc08
Administrator <295435732@qq.com>
2018-11-30 13:21:59 +0800

    init

---
+++ src/main/java/com/workflow/core/WorkflowCoreApplication.java
@@ -0,0 +1,12 @@
+package com.workflow.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowCoreApplication.class, args);
+	}
+}
