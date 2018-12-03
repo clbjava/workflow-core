74ed1081c9a4b91607766200404925cb86f2a12e
Administrator <295435732@qq.com>
2018-12-03 18:14:33 +0800

    step-9

--- src/test/java/com/base/test/BaseTest.java
+++ src/test/java/com/base/test/BaseTest.java
@@ -1,6 +1,8 @@
 package com.base.test;
 
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @Title: workflow-core
@@ -9,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @date: 2018-12-03 14:27
  * @version: V1.0
  */
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class BaseTest {
 }
