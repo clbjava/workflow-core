196670de2ec3acb6029f36107a1c414613500379
Administrator <295435732@qq.com>
2018-12-03 14:33:39 +0800

    step-8

---
+++ src/test/java/com/base/test/BaseTest.java
@@ -0,0 +1,14 @@
+package com.base.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Title: workflow-core
+ * @Description: TODO
+ * @author: Administrator
+ * @date: 2018-12-03 14:27
+ * @version: V1.0
+ */
+@SpringBootTest
+public class BaseTest {
+}
